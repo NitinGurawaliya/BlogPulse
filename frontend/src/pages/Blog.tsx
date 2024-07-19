0ffe944dc0f1a8cf660485a532a21ea4ed00cb9b
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-07-19 21:36:05 +0530

    Update Blog.tsx

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,29 +1,3 @@
-// import { Appbar } from "../components/Appbar"
-// import { FullBlog } from "../components/FullBlog"
-// import { useBlog } from "../hooks" 
-// import { useParams } from "react-router-dom"
-
-
-// export const Blog = () => {
-//   const {id} = useParams()
-//   const {loading,blog} = useBlog({id:id || ""})
-//   if(loading){
-//     return <div>
-//       <Appbar />
-   
-//     <div className="h-screen flex flex-col justify-center">
-//         <div className="flex justify-center">
-//           <Spinner />
-//             </div>
-//         </div>
-//         </div>
-//   } 
-//   return (
-//     <div>
-//       <FullBlog blog={blog} />
-//     </div>
-//   )
-// }
 
 import { Appbar } from "../components/Appbar"
 import { FullBlog } from "../components/FullBlog"
