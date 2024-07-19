5bb40b9bc1394d74ac5afbf4df91c3257aeef28c
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-07-19 21:35:56 +0530

    Update Blogs.tsx

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -13,7 +13,12 @@ export const Blogs = () => {
       <BlogSkeleton />
       <BlogSkeleton />
       <BlogSkeleton />
+
       <BlogSkeleton />
+      <BlogSkeleton />
+      <BlogSkeleton />
+      <BlogSkeleton />
+      
     </div>
     </div>
 
