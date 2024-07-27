9c3a217339fd24b0d5755e091c57188c2eb5a184
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-07-27 20:56:16 +0530

    Update blog.routes.ts

--- backend/src/router/blog.routes.ts
+++ backend/src/router/blog.routes.ts
@@ -19,30 +19,3 @@ blogRouter.post('/',authMiddleware,createBlog )
 blogRouter.put('/',authMiddleware,editBlog)
 blogRouter.get('/:id',authMiddleware,getBlogById)
 blogRouter.post("/comment/:id",authMiddleware,addComment)
-
-
-
-
-
-
-
-//middleware here to extract the userId from the jwt token provdexd 
-
-// blogRouter.use("/*",async(c,next)=>{
-
-//   const authHeader =  c.req.header("authorization") || "";
-//   const user =await verify(authHeader, c.env.JWT_SECRET)
-//   if(user){
-//     //@ts-ignore
-//     c.set("userId",user.id)
-//     await next()
-//   }
-//   else{
-//     c.status(403)
-//     return c.json({
-//       msg:"Not authorized "
-//     })
-//   }
-
-
-// })s
