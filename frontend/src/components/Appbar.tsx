2415c1fe692ee6ead4e8d561b4e647e3a2aca29e
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-07-28 00:29:32 +0530

    Update Appbar.tsx

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,18 +1,24 @@
-import { Link } from "react-router-dom"
-import { Avatars } from "./BlogCard"
+import { Link } from "react-router-dom";
+import { Avatars } from "./BlogCard";
+import Writelogo from "../assets/Writelogo";
 
 export const Appbar = () => {
   return (
-    <div className="border-b py-4 flex justify-between px-10">
-        <Link to={"/blogs"} className="flex font-bold text-2xl flex-col justify-center cursor-pointer">
-        Medium 
-        </Link>
-        <div>
+    <div className="border-b py-4 flex justify-between items-center px-10">
+      <Link to={"/blogs"} className="font-bold text-2xl cursor-pointer">
+        Medium
+      </Link>
+      <div className="flex items-center gap-4">
         <Link to={"/publish"}>
-           <button type="button" className="text-white mr-4 bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4  focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">new</button>
+          <button
+            type="button"
+            className="focus:outline-none hover:bg-gray-100 rounded-3xl focus:ring-4 focus:ring-gray-100 font-medium flex items-center gap-2 text-sm px-5 py-2.5"
+          >
+            <Writelogo /> Write
+          </button>
         </Link>
-            <Avatars name="nitin" />
-        </div>    
+        <Avatars name="nitin" />
+      </div>
     </div>
-  )
+  );
 }
