980e665d2ba0f69528866ccb4df648a53a57cb99
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-08-01 12:27:37 +0530

    Update user.routes.ts

--- backend/src/router/user.routes.ts
+++ backend/src/router/user.routes.ts
@@ -1,9 +1,12 @@
 
 import { Hono } from "hono"
-import { signin, signup } from "../controller/userController"
+import { signin, signup ,getProfile} from "../controller/userController"
+import { authMiddleware } from "../middleware/user"
 
 export const userRouter = new Hono()
 
 
 userRouter.post('/signup',signup )
 userRouter.post('/signin',signin)
+
+userRouter.get('/myProfile',authMiddleware,getProfile)
