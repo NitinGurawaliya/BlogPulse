cfc5235ef65e54067f3e98d88dcaad2d0084693c
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-08-08 13:23:06 +0530

    Update Blogs.tsx

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,45 +1,47 @@
-import { Appbar } from "../components/Appbar"
-import { BlogCard } from "../components/BlogCard"
-import  BlogSkeleton from "../components/BlogSkeleton"
-import { useBlogs } from "../hooks"
+import { Appbar } from "../components/Appbar";
+import { BlogCard } from "../components/BlogCard";
+import BlogSkeleton from "../components/BlogSkeleton";
+import { TagsComponent } from "../components/TagsComponent";
+import { useBlogs } from "../hooks";
 
 export const Blogs = () => {
-  const {loading,blogs} = useBlogs()
+  const { loading, blogs } = useBlogs();
 
-  if(loading){
-    return <div>
-      <Appbar />
-     <div>
-      <BlogSkeleton />
-      <BlogSkeleton />
-      <BlogSkeleton />
-
-      <BlogSkeleton />
-      <BlogSkeleton />
-      <BlogSkeleton />
-      <BlogSkeleton />
-      
-    </div>
-    </div>
-
+  if (loading) {
+    return (
+      <div>
+        <Appbar />
+        <div>
+          <BlogSkeleton />
+          <BlogSkeleton />
+          <BlogSkeleton />
+          <BlogSkeleton />
+          <BlogSkeleton />
+          <BlogSkeleton />
+          <BlogSkeleton />
+        </div>
+      </div>
+    );
   }
-  return (<div> 
-    <Appbar />
-  <div className="flex justify-center">
-    <div >
-      {blogs.map(blogs=><BlogCard  
-        id={blogs.id}
-       authorName={blogs.author.name}
-       title={blogs.title}
-       content={blogs.content}
-       publishedDate={"23 July,2024"}
-      />)}
 
-
-      
+  return (
+    <div>
+      <Appbar />
+      <TagsComponent />
+      <div className="flex justify-center">
+        <div>
+          {blogs.map((blog) => (
+            <BlogCard
+              key={blog.id}
+              id={blog.id}
+              authorName={blog.author.name}
+              title={blog.title}
+              content={blog.content}
+              publishedDate={"23 July, 2024"}
+            />
+          ))}
+        </div>
+      </div>
     </div>
-    
-    </div>
-    </div>
-  )
-}
+  );
+};
